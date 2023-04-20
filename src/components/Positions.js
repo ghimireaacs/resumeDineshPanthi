bc26807cc6b3d15fc569aded72f29426fd664136
ghimireaacs <aacs.csu@gmail.com>
2023-04-21 07:22:54 +1000

    structuring Json for home page

---
+++ src/components/Positions.js
@@ -0,0 +1,127 @@
+export function academicPosition(){
+    const positions=[
+        {id:1,
+        ended:"Present",
+        started:1998,
+        title:"Associate Professor",
+        college:"Nepal Sanskrit University"},
+        {id:2,
+        ended:2014,
+        started:2011,
+        title:"Associate Professor",
+        college:"Nepal Sanskrit University"},
+        {id:3,
+        ended:1998,
+        started:1996,
+        title:"Associate Professor",
+        college:"Nepal Sanskrit University"},
+        {id:4,
+        ended:2002,
+        started:2000,
+        title:"Associate Professor",
+        college:"Nepal Sanskrit University"},
+        {id:5,
+        ended:1995,
+        started:1994,
+        title:"Associate Professor",
+        college:"Nepal Sanskrit University"},
+        {id:6,
+        ended:1999,
+        started:1994,
+        title:"Associate Professor",
+        college:"Nepal Sanskrit University"},
+        {id:7,
+        ended:1991,
+        started:1989,
+        title:"Associate Professor",
+        college:"Nepal Sanskrit University"},
+        {id:8,
+        ended:1987,
+        started:1985,
+        title:"Associate Professor",
+        college:"Nepal Sanskrit University"},
+                
+    ]
+    console.log(positions)
+}
+
+export function education(){
+    const educations=[
+        {id:1,
+        title:"Ph.D.",
+        year:2013,
+        subject:"Ph.D. in Mathematics",
+        university:"Kathmandu University, School of Science"},
+        {id:2,
+        title:"",
+        year:"",
+        subject:"",
+        university:""},
+        {id:3,
+        title:"",
+        year:"",
+        subject:"",
+        university:""},
+        {id:4,
+        title:"",
+        year:"",
+        subject:"",
+        university:""},
+        {id:5,
+        title:"",
+        year:"",
+        subject:"",
+        university:""},
+        {id:6,
+        title:"",
+        year:"",
+        subject:"",
+        university:""},
+        {id:7,
+        title:"",
+        year:"",
+        subject:"",
+        university:""},
+        
+
+    ]
+    console.log(educations);
+}
+
+    export function honors={
+        const awards=[
+            {id:1,
+            year:2014,
+            title:"Nepal Vidyabhushan Ka",
+            description:"This title was awarded by Ministry of Education, Nepal Government."},
+            {
+            id:2,
+            year:2009,
+            title:"",
+            description:""},
+            {
+            id:2,
+            year:2009,
+            title:"",
+            description:""},
+            {
+            id:2,
+            year:2009,
+            title:"",
+            description:""},
+            {
+            id:2,
+            year:2009,
+            title:"",
+            description:""},
+            {
+            id:2,
+            year:2009,
+            title:"",
+            description:""},
+            
+        ]
+        console.log(awards);
+    }
+
+;
